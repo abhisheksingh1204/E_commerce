3fe7711646be37abca2eb837e4908ee1d8be7d2b
abhisheksingh1204 <chouhanabhishek9630@gmail.com>
2025-06-02 11:22:15 +0530

    my first commit

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "127.0.0.1",
+      database: "ecommerce",
+      user: "postgres",
+      password: "abhishek",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "users",
+    },
+  },
+};
